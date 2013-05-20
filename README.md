60941c090e8ca3866057a1b319389367c121306b
aqingsao <xqzhang@thoughtworks.com>
2013-05-20 21:30:00 +0800

    Add readme.md

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 java-socket-test
 ================
 
-Connecting to a server with batch sockets
+A simple Java socket client to test TCP connections to a server.
+
+### Setting up project
+This project uses Intellij IDEA as IDE and gradle as build script, to set it up, you could:
+* Fork code in github or clone the repository to your local disk;
+* Open IDEA and use "Import Project..."; Or you could run "gradle cleanIdea idea"
+* Then you could adjust parameters in Main.java and run main() method;
+
+### Usage
+* Host: the host of the server you want to connect;
+* Port: The port of the server you want to connect;
+* ClientCount: How many socket clients you want to create, socket client will open a TCP connection to the server;
+* ConnectIntervalInSeconds: How many seconds should sleep before creating a new socket client, if this interval is too short, socket might fail to open connection.
+
+
--- src/main/java/com/thoughtworks/Main.java
+++ src/main/java/com/thoughtworks/Main.java
@@ -2,18 +2,13 @@ package com.thoughtworks;
 
 public class Main {
 
-    private void run() {
-        String host = "10.18.2.163";
-        int port = 7222;
+    private void run(String host, int port, int clientCount, int connectIntervalInSeconds) {
 
-        int count = 240;
-        int intervalInSeconds = 15;
-
-        SocketClients clients = new SocketClients(count, intervalInSeconds);
+        SocketClients clients = new SocketClients(clientCount, connectIntervalInSeconds);
         clients.tryToConnect(host, port);
 
-        if (keepThisProcessForAWhile(count, clients)) {
-            int timeWillBeSpent = count * 15; // 240 will spend 1 hour(240 * 15=3600s)
+        if (keepThisProcessForAWhile(clientCount, clients)) {
+            int timeWillBeSpent = clientCount * connectIntervalInSeconds; // 240 will spend 1 hour(240 * 15=3600s)
             Utils.log("Will sleep for %d seconds before close all sockets", timeWillBeSpent);
             Utils.sleepInSeconds(timeWillBeSpent);
         }
@@ -25,7 +20,13 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        new Main().run();
+        String host = "10.18.2.163";
+        int port = 7222;
+
+        int clientCount = 240;
+        int connectIntervalInSeconds = 15;
+
+        new Main().run(host, port, clientCount, connectIntervalInSeconds);
     }
 
 }
