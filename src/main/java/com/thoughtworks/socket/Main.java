6c8ad852a890f0177d49e15208df2e01ce92b4e0
aqingsao <xqzhang@thoughtworks.com>
2013-05-21 10:59:49 +0800

    Will not stop process if no error occurred or connected sockets count is greater than 10

--- src/main/java/com/thoughtworks/socket/Main.java
+++ src/main/java/com/thoughtworks/socket/Main.java
@@ -8,22 +8,32 @@ public class Main {
         clients.tryToConnect(host, port);
 
         if (keepThisProcessForAWhile(clientCount, clients)) {
-            int sleepTime = clientCount * connectIntervalInSeconds; // 240 will spend 1 hour(240 * 15=3600s)
+            int sleepTime = 5 * 60 * 60; // will sleep 5 hours
             Utils.log("Will sleep for %d seconds before close all sockets", sleepTime);
             Utils.sleepInSeconds(sleepTime);
+        }
+        else{
+            Utils.log("Error occurred, or connected socket clients count is less than 10, we will close this process.");
         }
         clients.closeAll();
     }
 
     private boolean keepThisProcessForAWhile(int count, SocketClients clients) {
-        return !clients.isErrorOccurred() || count > 10; // We will
+        return !clients.isErrorOccurred() || count >= 10; // We will
     }
 
     public static void main(String[] args) {
+        if(args.length < 3){
+            Utils.log("Usage: java -jar java-socket-test.jar host:port clientCount connectIntervalInSeconds\n");
+            Utils.log("Params: clientCount is the total count of socket clients");
+            Utils.log("Params: connectIntervalInSeconds defines the interval before opening a new socket client");
+            Utils.log("Example: java -jar java-socket-test.jar 10.18.2.163:7222 120 15");
+        }
+
         String host = "10.18.2.163";
         int port = 7222;
 
-        int clientCount = 240;
+        int clientCount = 120;
         int connectIntervalInSeconds = 15;
 
         new Main().run(host, port, clientCount, connectIntervalInSeconds);
